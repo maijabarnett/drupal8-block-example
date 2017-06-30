ca7f8c0430be3863c84f8befbe702b2e756129de
Maija Barnett <maijabarnett@fs.fed.us>
2017-06-30 11:45:47 -0400

    Adding basiv drupal 8 block example and notes in README.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # drupal8-block-example
-How to make a block 
+How to make a block
+
+The src folder needs to go inside the folder for the module you created.
+. 
---
+++ src/Plugin/Block/TopOfPage.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Drupal\fs_core_glue\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'Hello' Block.
+ *
+ * @Block(
+ *   id = "top_of_page_home_buttons",
+ *   admin_label = @Translation("Top of Page & Home Buttons"),
+ *   category = @Translation("Top of Page & Home Buttons"),
+ * )
+ */
+class TopOfPage extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+  return array(
+      '#markup' => $this->t('Hello, World!'),
+    );
+  }
+}
